ef6244f8045b02bba1aca7f63e946d1d8f35739d
Kaylei Burke <kayleiburke@nypl.org>
2024-10-31 22:09:33 -0500

    Update contact information on Contact page

--- src/data/db/database.js
+++ src/data/db/database.js
@@ -472,9 +472,9 @@ const database = {
     },
   ],
   contactInfo: {
-    phoneNumbers: ["+012-3456-7891", "+012-3456-7892"],
-    emailAddress: ["admin.sitename@example.com", "info.sitename@example.com"],
-    address: "121 King Street, Melbourne, Victoria 3000, Australia",
+    phoneNumbers: ["+1 402-909-3286"],
+    emailAddress: ["kaylei.burke@gmail.com"],
+    address: "Omaha, Nebraska",
   },
 };
 
