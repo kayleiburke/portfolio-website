83a28f640d3d8adfc0a09b9017aa6c7ebf16fd58
Kaylei Burke <kayleiburke@nypl.org>
2024-11-01 02:22:23 -0500

    Slight update to resume page wording

--- src/pages/Resumes.js
+++ src/pages/Resumes.js
@@ -149,7 +149,7 @@ function Resumes() {
           <div className="mi-resume-area mi-section mi-padding-top mi-padding-bottom">
             <div className="container">
               <Sectiontitle title="Resume" />
-              <Smalltitle title="Working Experience" icon="briefcase" />
+              <Smalltitle title="Work Experience" icon="briefcase" />
               <div className="mi-resume-wrapper">
                 {workingExperience.map((workingExp) => (
                     <Resume key={workingExp.id} resumeData={workingExp} />
