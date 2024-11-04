8fffc7d464b4aa248c13404a733557b1010ec0a9
Kaylei Burke <kayleiburke@nypl.org>
2024-11-03 20:13:21 -0600

    Install and configure Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactGA from 'react-ga4';
 import * as Icon from "react-feather";
 import "./App.scss";
 import About from "./pages/About";
@@ -9,8 +10,11 @@ import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
 import Portfolios from "./pages/Portfolios";
 import Resumes from "./pages/Resumes";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, useLocation } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
+
+// Initialize the Google Analytics ID here
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID
 
 function App() {
   const [lightMode, setLightMode] = useState(false); // Made it true if you want to load your site light mode primary
@@ -18,6 +22,12 @@ function App() {
   lightMode
     ? document.body.classList.add("light")
     : document.body.classList.remove("light");
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    // Track the initial page load
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+  }, []);
 
   const handleMode = () => {
     if (!lightMode) {
